e5cef90870d5b09a500fb6ea22091fdef88a9eb8
Shubham-git-Soni <shubhamsoni7857@gmail.com>
2025-06-11 19:17:54 +0530

    i am adding this file

--- src/main/java/com/abhi/authProject/controller/HomeController.java
+++ src/main/java/com/abhi/authProject/controller/HomeController.java
@@ -7,7 +7,6 @@ import org.springframework.web.bind.annotation.RestController;
 import jakarta.servlet.http.HttpServletRequest;
 
 @RestController
-
 public class HomeController {
 
     @GetMapping("/index")
